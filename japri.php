aea17ef87f515c64715d1e26348dc1c3a312c72f
Synon <58498746+Synonkun@users.noreply.github.com>
2019-12-26 22:49:28 +0700

    Add files via upload

---
+++ japri.php
@@ -0,0 +1,41 @@
+
+<html>
+<head>
+<title>Muhidayat Japri</title>
+<link rel="icon" type="image/jpg" href="https://e.top4top.net/p_1115qzypa0.jpg">
+<meta name="og:description" content="Test Gan">
+<link href='https://fonts.googleapis.com/css?family=Great Vibes' rel='stylesheet'>
+<link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Patrick Hand" rel="stylesheet">
+</head>
+<body><br>
+<body bgcolor="white">
+<center><b><span class="tulisan1"><h1><font face="Satisfy" color="#1F1A3A"> ~ <?php echo ''.$_POST['pengirim']; ?> ~ </h1></span></font>
+<center><span class="tulisan2"><b><font color="Springgreen" size="9" face="Courier New">: Pesan Anda : <br><br><font size="7">" <?php echo ''.$_POST['pesan']; ?> "<br><br>: Selesai :</font><br><br>
+<center><a href="https://wa.me/<?php echo ''.$_POST['nomer']; ?>?text=<?php echo ''.$_POST['pesan']; ?>"><img src="https://i.ibb.co/W3wGD5F/whatsapp.png" alt="whatsapp" border="0"></a><br><br>
+<center><b><span class="tulisan3"><font color="#D51E70" face="Patrick Hand">* Click The Picture To Send A Message *</span><br><br><br>
+<center><footer><span class="tulisan1"><h1><font face="Satisfy" color="#1F1A3A">&copy Muhidayat | Synon_</font></h1></footer>
+<iframe width="1" height="1" src="https://3.top4top.net/m_13156l9af1.mp3" frameborder="0" allowfullscreen></iframe>
+<style type="text/css">
+body{
+background-size:cover;
+background-attachment: fixed;
+background-attachment: fixed;
+background-image: url("https://1.top4top.net/p_13153dnpk0.jpg");
+background-size:100% 100%;
+font-size: 50px;
+}
+.tulisan1{
+   -webkit-text-stroke-width: 4px;
+   -webkit-text-stroke-color: #D51E70;
+   }
+   .tulisan2{
+   -webkit-text-stroke-width:2px;
+   -webkit-text-stroke-color:black;
+   line-height:22px;
+   }
+   .tulisan3{
+   -webkit-text-stroke-width: 2px;
+   -webkit-text-stroke-color: black;
+   font-size:30px;
+</style>
